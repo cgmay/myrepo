0ac77580b0d31aafc8129536d6fe26fdb6d6fb4b
Chris May <cgmay@alumni.ubc.ca>
2016-01-26 11:29:00 -0800

    Graphing on Jan26

--- seminar03b.r
+++ seminar03b.r
@@ -19,3 +19,83 @@ str(p)
 ylab("Expression of eggBomb") +
   ggtitle("Scatterplot for expression levels"))
 
+nDat <- 
+  with(kDat,
+       data.frame(sidChar, sidNum, devStage, gType, crabHammer,
+                  probeset = factor(rep(c("eggBomb", "poisonFang"),
+                                        each = nrow(kDat))),
+                  geneExp = c(eggBomb, poisonFang)))
+
+str(nDat)
+(p <- ggplot(nDat, aes(crabHammer, geneExp, color = probeset)) +
+  geom_point())
+
+# add smoothing line
+(p <- p + stat_smooth(se = F))
+
+
+# there are 2 lines because the groups are defined in the data
+# layer. We can turn it into one line by specifying another
+# aesthetic in the new later
+
+(p <- ggplot(nDat, aes(crabHammer, geneExp, color = probeset)) +
+  geom_point() +
+  stat_smooth(se = F, aes(group = 1)))
+
+# separate plots
+(p <- ggplot(nDat, aes(crabHammer, geneExp)) +
+  geom_point() +
+  facet_wrap(~ probeset))
+
+# colour code by genotype
+(p <- ggplot(nDat, aes(crabHammer, geneExp, color = gType)) +
+  geom_point() +
+  facet_wrap(~ probeset))
+
+#colour code by deveopment stage
+(p <- ggplot(nDat, aes(crabHammer, geneExp, color = devStage)) +
+  geom_point() +
+  facet_wrap(~ probeset))
+
+# Stripplot
+oDat <- 
+  with(kDat,
+       data.frame(sidChar, sidNum, devStage, gType,
+                  probeset = factor(rep(c("crabHammer", "eggBomb",
+                                          "poisonFang"), each = nrow(kDat))),
+                  geneExp = c(crabHammer, eggBomb, poisonFang)
+                  ))
+str(oDat)
+# plotting expression of each probe set
+(p <- ggplot(oDat, aes(geneExp, probeset)) +
+  geom_point())
+
+# jitter spaces the points out for better visualization
+(p <- ggplot(oDat, aes(geneExp, probeset)) +
+  geom_point(position = position_jitter(height = 0.3)))
+
+# expression changes over course of development
+(p <- ggplot(oDat, aes(devStage, geneExp)) +
+  geom_point())
+
+# separated by probe set
+(p <- p + facet_wrap(~ probeset))
+
+# with genotype info
+(p <- p + aes(color = gType))
+
+# adding averages
+(p <- p + stat_summary(fun.y = mean, geom = "point", 
+                       shape = 4, size = 4))
+
+
+# Density plots
+(p <- ggplot(oDat, aes(geneExp)) +
+  geom_density())
+
+# another method
+(p <- ggplot(oDat, aes(geneExp)) +
+  stat_density(geom = "line", position = "identity"))
+
+# more similar to lattice, with points at the bottom
+
